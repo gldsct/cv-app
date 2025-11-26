b138a867f5e33ffb770bfb8b5f350f042f4be3f8
gldsct <188839581+gldsct@users.noreply.github.com>
2025-11-26 11:57:16 +0545

    Implement About component
    Implement an About component to render the webpage's "about us"
    informational section

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import "../styles/About.css";
+
+function About () {
+    return (
+        <section id = "about">
+            <h2>About</h2>
+            <p>Create a standout CV in minutes with this easy-to-use CV maker. Start with a professionally designed template guaranteed to impress employers, enter your details, and get your CV job-ready in a few simple clicks — leave worrying about how to make your CV standout and effective to us! Whether you are a student or a seasoned pro, this app streamlines the process of creating an attention-drawing CV that highlights your skills and experience.</p>
+        </section>
+    );
+}
+
+export { About };
