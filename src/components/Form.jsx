cfe82c959a7d79a80624b6930b0238960bc1e14c
gldsct <188839581+gldsct@users.noreply.github.com>
2025-12-02 09:46:02 +0545

    Create Form component
    Create a Form component that includes all form elements/fields.

---
+++ src/components/Form.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { Button } from "./Button.jsx";
+import { Input } from "./Input.jsx";
+import { Skills } from "./Skills.jsx";
+import { Experience } from "./Experience.jsx";
+import { Education } from "./Education.jsx";
+import { Modal } from "./Modal.jsx";
+import "../styles/Form.css";
+
+function Form () {
+    return (
+        <section id = "create-cv">
+            <h2>Create Your CV</h2>
+            <form>
+
+            </form>
+        </section>
+    );
+}
+
+export { Form };
