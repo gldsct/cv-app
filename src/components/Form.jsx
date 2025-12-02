72d61bab6ec886d90c4865c51caf3fda294065f6
gldsct <188839581+gldsct@users.noreply.github.com>
2025-12-02 10:27:54 +0545

    Add personal details form fields
    Implement form fields to input the user's personal details.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -8,11 +8,26 @@ import { Modal } from "./Modal.jsx";
 import "../styles/Form.css";
 
 function Form () {
+    const [personalDetails, setPersonalDetails] = useState({});
+
+    function onDetailsChange (event) {
+        setPersonalDetails({...personalDetails, [event.target.name]: event.target.value});    
+    }
+    
     return (
         <section id = "create-cv">
             <h2>Create Your CV</h2>
             <form>
+                <section className = "personal-details">
+                    <h3>Personal Details</h3>
 
+                    <ul>
+                        <li><Input label = "Name: " id = "full-name" name = "user-name" placeholder = "John Doe" onChange = {onDetailsChange} /></li>
+                        <li><Input type = "email" label = "Email: " id = "mail" name = "user-mail" placeholder = "John@gmail.com" onChange = {onDetailsChange} /></li>
+                        <li><Input type = "tel" label = "Contact Number: " id = "number" name = "user-phone" placeholder = "123-456-7890" onChange = {onDetailsChange} /></li>
+                        <li><Input label = "Website: " id = "website" name = "user-website" placeholder = "github.com/name" onChange = {onDetailsChange} /></li>
+                    </ul>
+                </section>
             </form>
         </section>
     );
