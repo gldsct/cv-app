cf539829c494d1665bf41520298ef2e0e9fa3731
gldsct <188839581+gldsct@users.noreply.github.com>
2025-12-10 21:41:40 +0545

    Implement Footer component
    Implement Footer component to render the webpage's footer section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import "../styles/Footer.css";
+
+function Footer () {
+    return (
+        <footer>
+            <p>&copy; <a href = "https://github.com/gldsct">gldsct</a> (2025)</p>
+        </footer>
+    );
+}
+
+export { Footer };
