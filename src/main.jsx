c967c9bdd01754f74f01edf97726d1cf6246558c
gldsct <188839581+gldsct@users.noreply.github.com>
2025-12-13 21:18:02 +0545

    Import and render react components

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { Header } from "./components/Header.jsx";
+import { Hero } from "./components/Hero.jsx";
+import { Form } from "./components/Form.jsx";
+import { About } from "./components/About.jsx";
+import { Footer } from "./components/Footer.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Header label = {"main-navigation"} />
+    <Hero />
+    <Form />
+    <About />
+    <Footer />
   </StrictMode>
 )
